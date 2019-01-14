120026aa3c1d2d71da1f5d2257fbdde94113e09a
hloydl <harald.loydl@loydl.ch>
2019-01-14 10:03:37 +0100

    access Camunda REST api

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # camunda-spring-docker-demo1
 Spring Boot 3.2 - Camunda 7.10.0 - Docker - Demo 1  
 
+Installation<br>
 `mvn clean install`<br>
 `docker build -t loydlconsulting/cwp-mabritec . `<br>
 `docker run -p 8080:8080 loydlconsulting/cwp-mabritec`<br>
+
+REST API<br>
+`http://localhost:8080/rest/engine`
--- simpleweb/index.js
+++ simpleweb/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 
+// access Camunda REST API demo:
+// returns the first Camunda engine name from a possible list of engine names
 app.get('/', (req, res) => {
-    res.send('How are you doing');
+    fetch('http://cwp-mabritec-server:8080/rest/engine')
+    .then(rest_response => rest_response.json())
+    .then((json) => {
+        console.log(json);
+        res.send("Camunda REST API: first engine name='" + json[0].name + "'");
+    })
+    .catch(err => console.error(err));
 });
 
 app.listen(8050, () => {
